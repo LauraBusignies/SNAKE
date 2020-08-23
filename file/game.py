27b87e97f6b545cf68fa98a4abb2e7d0ec8aa787
LauraBusignies <67093562+LauraBusignies@users.noreply.github.com>
2020-08-23 17:53:42 +0200

    move strat, move end, can't come back

--- file/game.py
+++ file/game.py
@@ -15,6 +15,8 @@ class Game:
         # definir le background
         self.background = pygame.image.load('assets/background/background.png')
         self.background = pygame.transform.scale(self.background, (1080, 720))
+        self.moove_snake = True 
+        self.var_rotate_end = 0
 
     def start(self):
         self.is_playing = True
@@ -31,44 +33,84 @@ class Game:
         for bloc in self.snake.list_body:
             screen.blit(bloc.image, bloc.rect)
 
+    def turn_around(self, event):
+        self.moove_snake = True 
+
+        if self.snake.list_direction[len(self.snake.list_direction) -1] == "Y-" and event == "bas" :
+            self.moove_snake = False 
+        elif self.snake.list_direction[len(self.snake.list_direction) -1] == "Y+" and event == "haut" :
+            self.moove_snake = False 
+        elif self.snake.list_direction[len(self.snake.list_direction) -1] == "X-" and event == "droite" :
+            self.moove_snake = False 
+        elif self.snake.list_direction[len(self.snake.list_direction) -1] == "X+" and event == "gauche" :
+            self.moove_snake = False 
+        return self.moove_snake
+
+    def rotate_end(self):
+        self.var_rotate_end = 0
+        if self.snake.list_direction[2] == "X+" :
+            self.var_rotate_end = 0
+        elif self.snake.list_direction[2] == "X-" :
+            self.var_rotate_end = 180
+        elif self.snake.list_direction[2] == "Y+" :
+            self.var_rotate_end = -90
+        else : 
+            self.var_rotate_end = 90
+        
+
     def moove (self, event):
-        new_Y = self.snake.list_body[len(self.snake.list_body) -1].rect.y
-        new_X = self.snake.list_body[len(self.snake.list_body) -1].rect.x
-        new_rotate = 0
-        
-        if event == "haut":
-            self.snake.list_direction.append("Y-")
-            new_Y -= 40
-        elif event == "bas":
-            self.snake.list_direction.append("Y+")
-            new_Y += 40
-        elif event == "gauche" :
-            self.snake.list_direction.append("X-")
-            new_X -= 40
-        else:
-            self.snake.list_direction.append("X+")
-            new_X += 40
+        self.turn_around(event)
+        self.rotate_end()
+        if self.moove_snake :
+
+            new_Y = self.snake.list_body[len(self.snake.list_body) -1].rect.y
+            new_X = self.snake.list_body[len(self.snake.list_body) -1].rect.x
+            new_rotate = 0
+
+                    # supprime le dernier bloc du serpent
+            del self.snake.list_body[0]
+            del self.snake.list_direction[0]
+            # affiche la queue au bout de notre serpent
+            self.snake.list_body[0].image = pygame.image.load("assets/snake/end.png")
+            #self.snake.list_body[0].image = pygame.transform.scale(self.snake.list_body[0].image, (40, 40))
+            self.snake.list_body[0].image = pygame.transform.rotate(self.snake.list_body[0].image, self.var_rotate_end)
             
-        if self.snake.list_direction[len(self.snake.list_direction) - 1] == self.snake.list_direction[len(self.snake.list_direction) - 2]:
-            if self.snake.list_direction[len(self.snake.list_direction) - 2][0] == "X":
-                new_rotate = 0
+            self.snake.list_body[len(self.snake.list_body) -1].image = pygame.image.load("assets/snake/middle.png")
+            #self.snake.list_body[len(self.snake.list_body) -1].image = pygame.transform.scale(self.snake.list_body[len(self.snake.list_body) -1].image, (40, 40))
+            self.snake.list_body[len(self.snake.list_body) -1].image = pygame.transform.rotate(self.snake.list_body[len(self.snake.list_body) -1].image, new_rotate)
+            
+            if event == "haut":
+                self.snake.list_direction.append("Y-")
+                new_Y -= 40
+                self.snake.list_body.append(Snake_piece(new_X, new_Y, "assets/snake/start.png", 90))
+
+            elif event == "bas":
+                self.snake.list_direction.append("Y+")
+                new_Y += 40
+                self.snake.list_body.append(Snake_piece(new_X, new_Y, "assets/snake/start.png", -90))
+
+            elif event == "gauche" :
+                self.snake.list_direction.append("X-")
+                new_X -= 40
+                self.snake.list_body.append(Snake_piece(new_X, new_Y, "assets/snake/start.png", 180))
+
             else:
-                if self.snake.list_direction[len(self.snake.list_direction) - 2][1] == "+":
-                    new_rotate = 270
+                self.snake.list_direction.append("X+")
+                new_X += 40
+                self.snake.list_body.append(Snake_piece(new_X, new_Y, "assets/snake/start.png", 0))
+                
+            if self.snake.list_direction[len(self.snake.list_direction) - 1] == self.snake.list_direction[len(self.snake.list_direction) - 2]:
+                if self.snake.list_direction[len(self.snake.list_direction) - 2][0] == "X":
+                    new_rotate = 0
                 else:
-                    new_rotate = 90
-        
-        # supprime le dernier bloc du serpent
-        del self.snake.list_body[0]
-        # affiche la queue au bout de notre serpent
-        self.snake.list_body[0].image = pygame.image.load("assets/snake/end.png")
-        self.snake.list_body[0].image = pygame.transform.scale(self.snake.list_body[0].image, (40, 40))
-        
-        self.snake.list_body[len(self.snake.list_body) -1].image = pygame.image.load("assets/snake/middle.png")
-        self.snake.list_body[len(self.snake.list_body) -1].image = pygame.transform.scale(self.snake.list_body[len(self.snake.list_body) -1].image, (40, 40))
-        self.snake.list_body[len(self.snake.list_body) -1].image = pygame.transform.rotate(self.snake.list_body[len(self.snake.list_body) -1].image, new_rotate)
-        
-        self.snake.list_body.append(Snake_piece(new_X, new_Y, "assets/snake/start.png", new_rotate))
+                    if self.snake.list_direction[len(self.snake.list_direction) - 2][1] == "+":
+                        new_rotate = 270
+                    else:
+                        new_rotate = 90
+            
+
+            
+            # self.snake.list_body.append(Snake_piece(new_X, new_Y, "assets/snake/start.png", new_rotate))
 
 
-# list_2_d = ["Y-", "X+", "X+"]
+    # list_2_d = ["Y-", "X+", "X+"]
